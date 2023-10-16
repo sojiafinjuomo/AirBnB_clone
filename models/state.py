021836412c27aef20ff60f3b2bc250210df59bdf
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-10-16 15:01:47 -0700

    state.py

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+The child class State inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+clas State(BaseModel):
+    """
+    Public class attributes:name is empty string
+    """
+    name = ""
