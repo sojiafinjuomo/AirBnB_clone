bcacf4f4dd79c1302a1765733dcdee6732c930ad
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-10-16 14:14:52 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
         else:
             for key, value in kwargs.item():
                 if key == 'created_at' or key == 'updated_at':
@@ -42,7 +42,7 @@ class BaseModel():
         updates the public instance attribute
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
