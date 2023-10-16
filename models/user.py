7bd934b0bc04d33c177991883a80d8a1d55ac7b1
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-10-16 14:39:55 -0700

    user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """
 User Class (inherits from BaseModel)
-Author: Mire
+Author: Soji & Mire
 """
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
+    """
+    user child class inherits from BaseModel
+    """
     email = ''
     password = ''
     first_name = ''
