61c3c33a38afc7b8e1ebae9290aad8d47d7fe5c6
mire-web <amahe8664@gmail.com>
2023-10-16 04:46:01 +0100

    Update Console with more Functions

--- console.py
+++ console.py
@@ -4,25 +4,40 @@ Module for a class HBNBCommand
 """
 
 import cmd
+from models import storage
+from models.base_model import BaseModel
+import json
+objects = storage.all()
 
 
 class HBNBCommand(cmd.Cmd):
     """
     contains the entry point of the command interpreter
     """
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
     def do_greet(self, line):
-        """greet [line]
-             greet the named person"""
+        """
+        Greets the User
+        Usage: greet <user>
+        """
         if line:
             print("hi", line)
         else:
             print("hi")
 
     def do_quit(self, line):
-        """Quit command to exit the progrem"""
+        """
+        Quit command to exit the program
+        Usage: quit
+        """
         return True
+
+    def do_exit(self, line):
+        """
+        Exits the program
+        Usage: exit
+        """
 
     def emptyline(self):
         """shouldn't execute anything"""
@@ -31,6 +46,99 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         """ Exit the program"""
         return True
+
+    def do_create(self, line=None):
+        """
+        Creates new instance of BaseModel
+        Usage: create <class_name>
+        """
+        if not line:
+            print("** class name missing **")
+        elif line != 'BaseModel':
+            print("** class doesn't exist **")
+        else:
+            new_model = BaseModel()
+            new_model.save()
+            print(new_model.id)
+
+    def do_show(self, line=None):
+        """
+        Prints String representation of an Instance
+        Usage: show <class_name> <id>
+        """
+        args = line.split()
+        args.append(None)
+        if not args[0]:
+            print('** class name missing **')
+        elif args[0] != 'BaseModel':
+            print("** class doesn't exist**")
+        elif not args[1]:
+            print("** instance id missing **")
+        elif f'{args[0]}.{args[1]}' not in objects:
+            print("** no instance found **")
+        else:
+            model = objects[f'{args[0]}.{args[1]}']
+            print(model)
+
+    def do_delete(self, line):
+        """
+        Deletes Instance from storage
+        Usage: delete <class_name> <id>
+        """
+        args = line.split()
+        args.append(None)
+        if not args[0]:
+            print('** class name missing **')
+        elif args[0] != 'BaseModel':
+            print("** class doesn't exist**")
+        elif not args[1]:
+            print("** instance id missing **")
+        elif f'{args[0]}.{args[1]}' not in objects:
+            print("** no instance found **")
+        else:
+            del objects[f'{args[0]}.{args[1]}']
+            with open("./models/engine/storage.json", 'w') as file:
+                json.dump(objects, file)
+
+    def do_all(self, line):
+        """Prints the string representation of all instances
+        Usage:
+           all <class_name>(optional)
+        """
+        models = []
+        if line != '' and line != 'BaseModel':
+            print("** class doesn't exist **")
+        else:
+            for key, value in objects.items():
+                sep = key.split('.')
+                models.append(f'[{sep[0]}] ({sep[1]}) {value}')
+            print(models)
+
+    def do_update(self, line):
+        """Update object attributes or add attributes to object
+
+        Usage:
+            update <class_name> <id> <attribute name>
+            <attribute value>
+        """
+        args = line.split()
+        args.append(None)
+        if not args[0]:
+            print('** class name missing **')
+        elif args[0] != 'BaseModel':
+            print("** class doesn't exist**")
+        elif not args[1]:
+            print("** instance id missing **")
+        elif f'{args[0]}.{args[1]}' not in objects:
+            print("** no instance found **")
+        elif not args[2]:
+            print("** attribute name missing **")
+        elif not args[3]:
+            print("** value missing **")
+        else:
+            objects[f'{args[0]}.{args[1]}'][f'{args[2]}'] = f'{args[3]}'
+            with open('./models/engine/storage.json', 'w') as file:
+                json.dump(objects, file)
 
 
 if __name__ == "__main__":
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -13,8 +13,15 @@ file_path = './models/engine/storage.json'
 
 
 class TestFileStorage(unittest.TestCase):
+    """Tests the FileStorage Class and methods
+
+	Args:
+		unittest (Class): creates unittests
+	"""
+
     @classmethod
     def setUpClass(cls):
+        """Sets up testing environment"""
         cls.my_model_1 = BaseModel()
         cls.my_model_2 = BaseModel()
         cls.my_model_1.name = 'mire'
@@ -26,14 +33,17 @@ class TestFileStorage(unittest.TestCase):
         storage.reload()
 
     def test_all(self):
+        """Tests the all method that it returns the correct data
+        """
         all_object = storage.all()
         self.assertEqual(type(all_object), dict)
-        class_name = f'{self.my_model_1.__class__.__name__}
-        id_num = {self.my_model_1.id}'
+        class_name = f'{self.my_model_1.__class__.__name__}'
+        id_num = f'{self.my_model_1.id}'
         key_content = f'{class_name}.{id_num}'
         self.assertEqual(type(all_object[f'{key_content}']), dict)
 
     def test_new(self):
+        """Tests new that it adds new obj to dictionary"""
         new_model = BaseModel()
         new_model.city = "lagos"
         new_model.save()
@@ -41,6 +51,7 @@ class TestFileStorage(unittest.TestCase):
         self.assertIn(f'{model_name}', storage.all())
 
     def test_storage_save(self):
+        """Tests that the save function in storage store fine"""
         storage.save()
         self.assertTrue(os.path.isfile(file_path))
         if os.path.isfile(file_path):
@@ -49,6 +60,7 @@ class TestFileStorage(unittest.TestCase):
                 self.assertEqual(data, storage.all())
 
     def test_reload(self):
+        """Tests the reload function"""
         data = {}
         with open(file_path, 'w') as file:
             json.dump(data, file)
@@ -59,6 +71,7 @@ class TestFileStorage(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
+        """Close testing environment"""
         del cls.my_model_1
         del cls.my_model_2
         Path("./models/engine/storage.json").unlink()
