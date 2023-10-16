c5842ea30d8dc9cd5ddb094d590e78f4a7b64e08
mire-web <amahe8664@gmail.com>
2023-10-16 05:34:39 +0100

    User Class Added

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
         else:
             for key, value in kwargs.item():
                 if key == 'created_at' or key == 'updated_at':
@@ -42,7 +42,7 @@ class BaseModel():
         updates the public instance attribute
         """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """
@@ -51,6 +51,6 @@ class BaseModel():
         """
         cp_dict = dict(self.__dict__)
         cp_dict['__class__'] = self.__class__.__name__
-        cp_dict[created_at] = self.created_at.strptime(format)
-        cp_dict[updated_at] = self.updated_at.strptime(format)
+        cp_dict['created_at'] = self.created_at.strptime(format)
+        cp_dict['updated_at'] = self.updated_at.strptime(format)
         return (cp_dict)
